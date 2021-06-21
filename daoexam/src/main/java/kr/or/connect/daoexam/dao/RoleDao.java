bf4ed06498c778312b0f45f15c621cc4adea638c
JaneKangOfficial <rkdto89@naver.com>
2021-06-21 19:48:07 +0900

    20210621 JDBC CRUD

--- daoexam/src/main/java/kr/or/connect/daoexam/dao/RoleDao.java
+++ daoexam/src/main/java/kr/or/connect/daoexam/dao/RoleDao.java
@@ -1,28 +1,37 @@
 package kr.or.connect.daoexam.dao;
 
+import static kr.or.connect.daoexam.dao.RoleDaoSqls.*;
+
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 import javax.sql.DataSource;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import kr.or.connect.daoexam.dto.Role;
-import static kr.or.connect.daoexam.dao.RoleDaoSqls.*;
 
 @Repository
 public class RoleDao {
 
 	// NamedParameterJdbcTemplate : 이름을 이용해서 바인딩하거나 결과 값을 가져올 때 사용 
 	private NamedParameterJdbcTemplate jdbc;
+	private SimpleJdbcInsert insertAction;
 	private RowMapper<Role> rowMapper = BeanPropertyRowMapper.newInstance(Role.class);
 	
 	// DBConfig.java 의 @Bean인 dataSource를 받아온다  
 	public RoleDao(DataSource dataSource) {
 		this.jdbc = new NamedParameterJdbcTemplate(dataSource);
+		this.insertAction = new SimpleJdbcInsert(dataSource)
+				.withTableName("role");
 	}
 
 	// static import 사용으로 클래스 이름 선언없이 SELECT_ALL 바로 사용 
@@ -30,4 +39,29 @@ public class RoleDao {
 		return jdbc.query(SELECT_ALL, Collections.emptyMap(), rowMapper);
 	}
 
+	public int insert(Role role) {
+		SqlParameterSource params = new BeanPropertySqlParameterSource(role);
+		return insertAction.execute(params);
+	}
+	
+	public int update(Role role) {
+		SqlParameterSource params = new BeanPropertySqlParameterSource(role);
+		return jdbc.update(UPDATE, params);
+	}
+	
+	public int deleteById(Integer id) {
+		Map<String, ?> params = Collections.singletonMap("roleId", id);
+		return jdbc.update(DELETE_BY_ROLE_ID, params);
+	}
+	
+	public Role selectById(Integer id) {
+		try {
+			Map<String, ?> params = Collections.singletonMap("roleId", id);
+			return jdbc.queryForObject(SELETE_BY_ROLE_ID, params, rowMapper);
+		}catch(EmptyResultDataAccessException e) {
+			return null;
+		}
+	}
+	
+	
 }
--- daoexam/src/main/java/kr/or/connect/daoexam/dao/RoleDaoSqls.java
+++ daoexam/src/main/java/kr/or/connect/daoexam/dao/RoleDaoSqls.java
@@ -4,4 +4,10 @@ public class RoleDaoSqls {
 
 	public static final String SELECT_ALL = "SELECT role_id, description FROM role ORDER BY role_id";
 	
+	public static final String UPDATE = "UPDATE role set description = :description WHERE role_id = :roleId";
+	
+	public static final String SELETE_BY_ROLE_ID = "SELECT role_id, description FROM role WHERE role_id = :roleId";
+
+	public static final String DELETE_BY_ROLE_ID = "DELETE FROM role WHERE role_id = :roleId";
+	
 }
