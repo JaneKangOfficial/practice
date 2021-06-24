d96949b7b40777d968e57a543bc5a84c2267d1ab
JaneKangOfficial <rkdto89@naver.com>
2021-06-24 18:59:26 +0900

    20210624 commit

---
+++ mvcexam/src/main/java/kr/or/connect/mvcexam/dto/User.java
@@ -0,0 +1,33 @@
+package kr.or.connect.mvcexam.dto;
+
+public class User {
+
+	private String name;
+	private String mail;
+	private int age;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", mail=" + mail + ", age=" + age + "]";
+	}
+	
+}
