4320f031e90cb1dd742b4ef5bf9c97be540fb3b3
JaneKangOfficial <rkdto89@naver.com>
2021-06-25 15:08:55 +0900

    20210625 commit

---
+++ guestbook/src/main/java/kr/or/connect/guestbook/config/DBConfig.java
@@ -0,0 +1,51 @@
+package kr.or.connect.guestbook.config;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+
+@Configuration
+/*
+ 	@EnableTransactionManagement
+	 transaction과 관련된 설정을 자동으로 해준다. 
+	 단, 사용자간의 트랜잭션 처리를 위한 PlatformTransactionManager를 설정하기 위해서는 
+	 TransactionManagementConfigurer를 구현하고 annotationDrivenTransactionManager 메서드를 오버라이딩 해야 한다. 
+	 PlatformTransactionManager를 return 한다. 
+ */
+@EnableTransactionManagement
+public class DBConfig implements TransactionManagementConfigurer {
+	
+	private String driverClassName = "com.mysql.jdbc.Driver";
+
+	private String url = "jdbc:mysql://localhost:3306/connectdb?useUnicode=true&characterEncoding=utf8";
+
+	private String username = "connectuser";
+
+	private String password = "connect123!@#";
+
+	@Bean
+	public DataSource dataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName(driverClassName);
+		dataSource.setUrl(url);
+		dataSource.setUsername(username);
+		dataSource.setPassword(password);
+		return dataSource;
+	}
+
+	@Override
+	public PlatformTransactionManager annotationDrivenTransactionManager() {
+		return transactionManger();
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManger() {
+		return new DataSourceTransactionManager(dataSource());
+	}
+}
---
+++ guestbook/src/main/java/kr/or/connect/guestbook/config/WebMvcContextConfiguration.java
@@ -0,0 +1,52 @@
+package kr.or.connect.guestbook.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+// DispatcherServlet이 읽는다. 
+
+// 설정 파일 
+@Configuration
+// 기본적인 설정 자동 
+@EnableWebMvc
+// controller 읽어 오기 
+@ComponentScan(basePackages = { "kr.or.connect.mvcexam.controller" })
+public class WebMvcContextConfiguration extends WebMvcConfigurerAdapter {
+
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/").setCachePeriod(31556926);
+        registry.addResourceHandler("/img/**").addResourceLocations("/img/").setCachePeriod(31556926);
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/").setCachePeriod(31556926);
+    }
+ 
+    // default servlet handler를 사용하게 합니다. (mapping 정보가 없는 url 요청이 들어왔을 때 DefaultServletHttpRequesetHandler가 처리하도록 한다.) 
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+   
+    // 특정 url에 대한 처리를 controller 작성하지 않고 mapping 할 수 있도록 한다. 
+    @Override
+    public void addViewControllers(final ViewControllerRegistry registry) {
+    		System.out.println("addViewControllers가 호출됩니다. ");
+        registry.addViewController("/").setViewName("index");
+    }
+    
+    // ViewResolver가 ViewName을 찾을 수 있도록 한다. 
+    @Bean
+    public InternalResourceViewResolver getInternalResourceViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+	
+}
